d5ee03597830b0dd2e43f584b81caa1261f8f09e
Holanad <volkov99.at@gmail.com>
2021-08-19 23:00:33 +0300

    feat: ready task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let enumeration = bracketsConfig.map(e => e.join(""));    
+    for (let i = 0; i < enumeration.length; i++) {
+        if (str.includes(enumeration[i])) {
+            str = str.replace([enumeration[i]], "");
+            i = -1;
+        }
+    }
+    return !str.length;
 }
